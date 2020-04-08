b745f3f47b6508212f6f9e04ada55292a7911ae9
ZeiGReflexx <33222571+ZeiGReflexx@users.noreply.github.com>
2020-04-08 13:58:28 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+keys = {
+        "name": "ChefPreme",
+        "email": "email@gmail.com",
+        "phone_number": "6789998212",
+        "street_address": "192B King George Avenue",
+        "city": "London", 
+        "zip_code": "91601",
+        "card_type": "Mastercard",
+        "card_cvv": "666",
+        "card_number": "1228199669918221",
+        "exp_month": "02",
+        "exp_year": "2021"
+}
